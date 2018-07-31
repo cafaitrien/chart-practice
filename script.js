b6cca824981a233bc53168310e5b542f6001077e
Michael Cobb <cobbmj@uw.edu>
2018-07-30 19:20:32 -0700

    added a second chart

--- script.js
+++ script.js
@@ -6,13 +6,8 @@ console.log("let's build some charts!")
 
   // Set a callback to run when the Google Visualization API is loaded.
   google.charts.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
+  google.charts.setOnLoadCallback(drawAllocationChart);
   function drawChart() {
-
-    // Create the data table.
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Score Range');
     data.addColumn('number', 'Games');
@@ -22,12 +17,28 @@ console.log("let's build some charts!")
       ['200-300', 3],
     ]);
 
-    // Set chart options
     var options = {'title':'Bowling Score Incidences',
                    'width':400,
                    'height':300};
 
-    // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+
+  function drawAllocationChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Score Range');
+    data.addColumn('number', 'Games');
+    data.addRows([
+      ['0-100', 2],
+      ['100-200', 5],
+      ['200-300', 3],
+    ]);
+
+    var options = {'title':'Bowling Score Incidences',
+                   'width':400,
+                   'height':300};
+
+    var chart = new google.visualization.PieChart(document.getElementById('allocation'));
     chart.draw(data, options);
   }
